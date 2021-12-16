04fa843833784ab76695032dd3fab3d136a9e4e2
nda666 <adhabakhtiar@gmail.com>
2021-12-16 18:20:22 +0700

    Set Avatar URL

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,6 @@ class User extends Authenticatable
 
     public function getAvatarAttribute($avatar)
     {
-        return $avatar ? Storage::url($avatar) : '';
+        return $avatar ? asset($avatar) : '';
     }
 }
