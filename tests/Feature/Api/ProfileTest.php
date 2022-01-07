98cfb1054689a8e6bbd3e2b68191abe95079fe7e
nda666 <adhabakhtiar@gmail.com>
2022-01-07 08:33:36 +0700

    Fix Typo

--- tests/Feature/Api/ProfileTest.php
+++ tests/Feature/Api/ProfileTest.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ProfileLoginTest extends TestCase
+class ProfileTest extends TestCase
 {
     use RefreshDatabase;
 
