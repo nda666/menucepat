a1bcc5b9c01507508e7e8931b5e7fef3d9b63abf
Adha Bakhtiar <adhabakhtiar@gmail.com>
2022-01-17 00:45:59 +0700

    add 3000 to schedule

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(52)->create();
         \App\Models\Announcement::factory(100)->create();
         \App\Models\Location::factory(10)->create();
-        \App\Models\Schedule::factory(10)->create();
+        \App\Models\Schedule::factory(3000)->create();
         // \App\Models\Family::factory(100)->create();
         \App\Models\Attendance::factory(100)->create();
     }
